59d13f40b91f6a51259312eb90a50b0cc6df3e1f
Vinicius <viniciuskarnopp777@hotmail.com>
2024-10-08 20:37:23 -0400

    Aula Dia 08-10-2024

--- src/App.java
+++ src/App.java
@@ -1,52 +1,9 @@
-import dominio.*;
 import visao.*;
-import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ClasseProdutoVisao visaoClasseProduto = new ClasseProdutoVisao();
-        SubclasseProdutoVisao visaoSubClasseProduto = new SubclasseProdutoVisao();
-        ProdutoVisao visaoProduto = new ProdutoVisao();
+        ClasseProdutoMenu menu = new ClasseProdutoMenu();
 
-        //Classe Produto
-
-        visaoClasseProduto.ExibirPorLinha();
-
-        ClasseProduto editar = new ClasseProduto();
-        editar.setCodigo(0);
-        editar.setDescricao("Editado");
-        editar.setDataDeInclusao(LocalDate.now());
-        visaoClasseProduto.Editar(3, editar);
-
-        visaoClasseProduto.Criar(editar);
-
-        visaoClasseProduto.Remover(2);
-
-        System.out.println();
-        System.out.println();
-
-        visaoClasseProduto.ExibirPorLinha();
-
-        //Sub Classe Produto
-        System.out.println("Sub Classes: ");
-
-        System.out.println();
-        System.out.println();
-
-        SubclasseProduto novoSubClasseProduto = new SubclasseProduto();
-        novoSubClasseProduto.setCodigoClasse(4);
-        novoSubClasseProduto.setDescricao("Em cacho");
-        novoSubClasseProduto.setDataDeInclusao(LocalDate.now());
-        visaoSubClasseProduto.Adicionar(novoSubClasseProduto);
-
-        visaoSubClasseProduto.Exibir();
-
-        //Produto
-        System.out.println("Produtos: ");
-
-        System.out.println();
-        System.out.println();
-
-        visaoProduto.Exibir();
+        menu.ExibirMenu();
     }
 }
